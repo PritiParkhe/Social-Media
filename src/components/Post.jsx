410a0649918da41f80ae6f00542469ac66d0212f
Priti Parkhe <parkhepriti47@gmail.com>
2024-05-18 16:27:25 +0530

    Components added Post, Create post, Header, Footer

---
+++ src/components/Post.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <div class="card post-card" style={{width: "18rem"}}>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <span class="badge text-bg-primary">Primary</span>
+
+      </div>
+    </div>
+  )
+}
+
+export default Post
